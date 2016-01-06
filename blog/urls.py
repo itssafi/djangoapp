ecea81e68cce64fd5ffb46ce0255f278afad6bd0
Safiulla Shaikh <safiulla.shaikh@accionlabs.com>
2016-01-07 00:23:18 +0530

    Fixing login error

--- blog/urls.py
+++ blog/urls.py
@@ -14,8 +14,9 @@ urlpatterns = patterns('',
 	url(r'^$', views.post_list, name='post_list'),
     # url(r'^$', 'django.contrib.auth.views.login'),
     url(r'^user/logout/$', views.logout_page, name='logout'),
-    url(r'^user/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
     url(r'^user/register/$', views.register, name='register'),
-    url(r'^user/register/success/$', views.register_success, name='register_success'),
+    url(r'^register/success/$', views.register_success, name='register_success'),
     url(r'^user/home/$', views.home, name='home'),
+    url(r'^home/$', views.home, name='home'),
 )
--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,14 @@ from django.utils import timezone
 from .models import Post
 from blog.forms import *
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
 from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
  
 @csrf_protect
 def register(request):
