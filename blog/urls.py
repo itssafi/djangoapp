c917c01f0b16806850fe417f14c1fa88562926df
Safiulla Shaikh <safiulla.shaikh@accionlabs.com>
2016-01-06 22:18:09 +0530

    Fixing 404

--- blog/urls.py
+++ blog/urls.py
@@ -9,13 +9,13 @@
 from django.conf.urls import patterns, include, url
 from . import views
 
- 
+
 urlpatterns = patterns('',
 	url(r'^$', views.post_list, name='post_list'),
     # url(r'^$', 'django.contrib.auth.views.login'),
-    url(r'^home/logout/$', views.logout_page, name='logout'),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
-    url(r'^register/$', views.register, name='register'),
-    url(r'^register/success/$', views.register_success, name='register_success'),
-    url(r'^home/$', views.home, name='home'),
+    url(r'^user/logout/$', views.logout_page, name='logout'),
+    url(r'^user/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
+    url(r'^user/register/$', views.register, name='register'),
+    url(r'^user/register/success/$', views.register_success, name='register_success'),
+    url(r'^user/home/$', views.home, name='home'),
 )
